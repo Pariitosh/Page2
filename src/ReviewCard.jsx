b624bb6c0658ebcfbf33a1baa3452243ff6e196e
Paritosh <61315926+Pariitosh@users.noreply.github.com>
2023-10-08 14:02:23 +0530

    Update ReviewCard.jsx

--- src/ReviewCard.jsx
+++ src/ReviewCard.jsx
@@ -2,11 +2,11 @@ import {Avatar, Button, Flex, Paper, Rating, Text, Title} from "@mantine/core";
 
 export default function ReviewCard({avatar,name,review}){
     return(
-        <Paper bg='#2c2b3f' h='30%' w='100%'>
-            <Flex h='100%' direction='column' m='3%' gap='15%'>
+        <Paper bg='#2c2b3f' h='30%' w='100%'>  {/* Main Box for review card */}
+            <Flex h='100%' direction='column' m='3%' gap='15%'>      {/* Outer flex box to align items vertically */}
             <Flex  justify='space-between' align='center' w='100%'>
                 <Flex gap='5%' align='center' w='40%'  >
-                <Avatar color='orange'>{avatar}</Avatar>
+                <Avatar color='orange'>{avatar}</Avatar>   {/* Setting the content according to the props recieved */}
                 <Text size={'md'} >{name}</Text>
                 </Flex>
                 <Rating defaultValue={5}/>
